9c792bcc9ea644c4a0d5cfa8c28d4aeba15ec29e
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-06-18 01:48:46 +0200

    Show info on Stats component

--- src/components/statistics/stats.jsx
+++ src/components/statistics/stats.jsx
@@ -26,38 +26,74 @@ const Stats = () => {
                     if( response.status !== 200 ) {
                         console.log(`Could not fetch ${stat.name} in Stats Component. Status: ` + response.status);
                     }
+                    
                     let json = await response.json();
-                    stat.setState(json);
-
                     console.log(json);   /** */
+
+                    if(json.hamsters || json.totalGames ) {
+                        stat.setState(json);
+                    }
+
+                    return json;
+
                 } catch (error) {
                     console.error(error);
                 }
             })
-       
         }
 
         fetchStats();
 
     }, [])
 
+    let topList = topHamsters ? 
+        <ol>
+            { topHamsters.hamsters.map(hamster => (
+                <li key={hamster.id}>{hamster.name} has won {hamster.wins} out of {hamster.games}</li>)) }
+        </ol>
+        : <p>We are currently unable to show you the top five hamsters.</p>;
+
+    let bottomList = losers ? 
+        <ol>
+            { losers.hamsters.map(hamster => (
+                <li key={hamster.id}>{hamster.name} has lost {hamster.defeats} out of {hamster.games}</li>)) }
+        </ol>
+        : <p> We are currently unable to show you the bottom five hamsters.</p>;
+
+
 
     return (
         <section>
             <h2>Hamster Wars - Statistics</h2>
-
+            
             <article>
                 <h3>Top 5 Hamsters</h3>
-
+                {topList}
             </article>
+
             <article>
                 <h3>Bottom 5 Hamsters </h3>
-
+                {bottomList}
             </article>
+
             <article>
                 <h3>Total Number of Battles</h3>
-                <p>{} games have been played to date.</p>
+                <p>
+                {
+                    totalGames ?
+                    `${totalGames.totalGames} games have been played to date.`
+                    : ''
+                }
+                </p>
             </article>
+
+            {
+                !losers && !topHamsters && !totalGames 
+                ? <div className="error-message"> 
+                    We are facing challenges loading Battle Statistics at the moment.
+                  </div>
+                : ''
+            }
         </section>
     )
 }
