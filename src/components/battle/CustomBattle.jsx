0cc3f0bcf1afed71bd9e7e694d88f9a229c8ed5c
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-06-17 02:12:05 +0200

    Post battle result in Custom Battle

--- src/components/battle/CustomBattle.jsx
+++ src/components/battle/CustomBattle.jsx
@@ -1,33 +1,80 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-const Battle = () => {
+const Battle = ({ props: { outcome, setOutcome, postMatchResult }}) => {
 
-    const [hamsters, setHamsters] = useState([]);
+    const [hamstersExist, setHamstersExist] = useState(false);
+    const [hamsters, setHamsters] = useState('');
+    const [winner, setWinner] = useState('');
 
-    const params = useParams();
+
     const { id1 } = useParams();
     const { id2 } = useParams();
 
-    console.log('params: ', params);
-    console.log('id1: ', id1);
-    console.log('id2: ', id2);
-
 
     useEffect(() => {
+
         let url = '/api/hamsters/';
 
-        setHamsters(fetchAllHamsters);
-        console.log(hamsters);
+        let fetchAllHamsters = async () => {
+         
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
         
+                return setHamsters(json.hamsters)
+                
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchAllHamsters();
 
     }, [setHamsters, hamsters])
 
+
+    useEffect(() => {
+
+        let id1Exists = (hamsters.some(hamster => hamster.id === Number(id1) ));
+        let id2Exists = (hamsters.some(hamster => hamster.id === Number(id2) ));
+
+        (id1Exists && id2Exists) ? setHamstersExist(true) : setHamstersExist(false);
+
+
+    }, [id1, id2, hamsters, setHamstersExist])
+
+
+    useEffect(()=> {
+
+        let loser = (winner === id1) ? Number(id2) : Number(id1);
+        
+        let result = {
+            win: winner,
+            defeat: loser
+        };
+
+        postMatchResult(result);
+        
+    }, [id1, id2, winner, postMatchResult])
+
+
     return(
         <section>
-            {/* params: { params } */}
-            id1: { id1 }
-            id2: { id2 }
+            <h2>Hamster Battle</h2>
+            <p>Click on the cutest hamster!</p>
+            <div className="competing-hamster">
+                {
+                    hamstersExist ?
+                        <><img src={'/assets/' + id1}     
+                            alt="competing hamster"
+                            onClick={ ()=> setWinner(Number(id1)) } />
+                        <img src={'/assets/' + id2}     
+                            alt="competing hamster"
+                            onClick={ ()=> setWinner(Number(id2)) } /></>
+                        : 'Please select valid hamsters between ....'
+                }
+            </div>
         </section>
     )
 }
@@ -35,15 +82,3 @@ const Battle = () => {
 export default Battle;
 
 
-let fetchAllHamsters = async () => {
-         
-    try {
-        const response = await fetch('/api/hamsters/');
-        const json = await response.json();
-
-        return (json.hamsters);
-        
-    } catch (error) {
-        console.error(error);
-    }
-}
